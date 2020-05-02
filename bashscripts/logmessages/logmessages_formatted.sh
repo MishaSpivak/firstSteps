09926f546d790c5cf84eac0bcf30b21a177d523f
Kartowka <mspivak1995@gmail.com>
2020-05-02 04:59:43 +0300

    add mistake eval

--- bashscripts/logmessages/logmessages_formatted.sh
+++ bashscripts/logmessages/logmessages_formatted.sh
@@ -8,30 +8,41 @@
 
 # Get every message from every even second
 message_from_even_second=$(sudo awk '$3 ~ /\<.{6}[0-6][0,2,4,6,8]/ { print }' /var/log/messages)
-#sudo awk '$3 ~ /\<.{6}[0-6][0,2,4,6,8]/ { print }' /var/log/messages > test1
 #echo "$message_from_even_second"
-# Get count of columns
+# Get count of columns and line
 count_of_columns_and_lines=$(echo "$message_from_even_second" | awk '$0=""NR":"NF')
-echo "$count_of_columns_and_lines"
-#count_of_columns=$('{print $2}' "$count_of_collumns_and_lines" | sort -n | tail -n 1)
+#echo "$count_of_columns_and_lines"
+
+# Get count of columns
+
+count_of_columns=$(echo "$count_of_columns_and_lines" | awk -F : '{print $2}'| sort -n | tail -n 1)
+
+echo "$count_of_columns"
 
 # get count of lines
-#count_of_lines=$(awk -F : '{print $1}' $count_of_collumns_and_lines | sort -n | tail -n 1 )
+count_of_lines=$(echo "$count_of_columns_and_lines" | awk -F : '{print $1}'| sort -n | tail -n 1)
+
+echo "$count_of_lines"
 
 # Get every odd word from that every message
 listPATH="$"
 symbol=$
 
-#for ((i = 1 ; i <= $count_of_columns ; i=i+2)); do
+for ((i = 1 ; i <= $count_of_columns ; i=i+2)); do
 	listPATH+=$i,$symbol
 done
 echo $listPATH
 
 # $listPATH = something like $1,$2,$, so last 2 symbols have to be cut
-cut_last_2_symbol=$(rev $listPATH | cut -c 3- | rev) 
-printW="sudo awk '{ print $cut_last_2_symbol }' $message_from_even_second"
-file_with_evensecond_and_odd_words=$(eval $printW)
+cut_last_2_symbol=$(echo "$listPATH" | rev | cut -c 3- | rev) 
+echo $cut_last_2_symbol
+printW=$(echo "$message_from_even_second" | awk '{ print $cut_last_2_symbol}')
 
+#can`t echo $printW, have mistake with awk '{ print $cut_last_2_symbol}')
+
+echo "$printW"
+#file_with_evensecond_and_odd_words=$(eval $printW)
+#echo "$file_with_evensecond_and_odd_words"
 # Replace all vowels with letter "s" in all these words
 sed -i "s/[aeiouAEIOUS]/s/g" $file_with_evensecond_and_odd_words
 # Count the letters "s" in resulting message
